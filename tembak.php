7a7487ecda7f1ae60941ed3394caa37528669814
pakdoy <61060621+pakdoy@users.noreply.github.com>
2020-02-15 01:54:28 +0700

    Gofood021120a

--- tembak.php
+++ tembak.php
@@ -55,7 +55,7 @@ echo " ===========================\n";
         echo color("green","+] Your access token : ".$token."\n\n");
         save("token.txt",$token);
         echo color("red","\n===========(REDEEM VOUCHER)===========");
-        echo "\n".color("yellow","!] Claim voc GOFOODSANTUY19");
+        echo "\n".color("yellow","!] Claim voc GOFOOD021101A");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
